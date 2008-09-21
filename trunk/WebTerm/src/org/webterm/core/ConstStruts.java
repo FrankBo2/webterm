0bdb13d0063933631086b47c68c91f32b498b61b
charles <charles@ef0eec68-8bfb-11dd-aa02-45bbecb07bf0>
2008-09-21 11:48:09 +0000

    Log in : Ok

---
+++ trunk/WebTerm/src/org/webterm/core/ConstStruts.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.webterm.core;
+
+/**
+ * Constant definition for struts target.
+ * 
+ * @author charles
+ */
+public final class ConstStruts {
+
+	/**
+	 * Constructor
+	 */
+	private ConstStruts() {
+		super();
+	}
+	
+	/** Declare the error target */
+	public static final String TARGET_ERROR = "ERROR"; //$NON-NLS-1$
+
+	/** Declare the success target */
+	public static final String TARGET_SUCCESS = "SUCCESS";//$NON-NLS-1$
+
+}
